549f94f0125883ef9157ebd753587bdb8e8ce3db
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2025-04-01 06:45:04 +0200

    update

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return jsonify({"message": "API di test funzionante!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
