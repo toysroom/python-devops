ade3cdd162743233437ebfad98dbade6b280d437
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2025-04-01 09:40:44 +0200

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify({"message": "API di test funzionante2333!"})
+    return jsonify({"message": "API di test funzionante!"})
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
