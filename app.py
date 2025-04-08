1d05aff164d70e826381fc7415981015f6cee431
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2025-04-08 13:56:07 +0200

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify({"message": "API di test funzionante!!!!!! ciao"})
+    return jsonify({"message": "API di test funzionante con terraform!"})
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 4000))
