5fd2cb4435bc7446fe34c5af0ac68dff0a71b18c
Alessandro Brugioni <alessandro.brugioni@gmail.com>
2025-06-18 16:12:41 +0200

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify({"message": "API di test funzionante su azure con trigger github!"})
+    return jsonify({"message": "API di test funzionante su azure con trigger github!!!!"})
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 4000))
